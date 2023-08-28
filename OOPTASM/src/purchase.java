baecc1e6e00882b843eb8d27cbc2421dedb7d320
raymond <raymondcwk-wm22@student.tarc.edu.my>
2023-08-28 11:08:27 +0800

    raymond

---
+++ OOPTASM/src/purchase.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.List;
+import java.util.Scanner;
+public class purchase extends itemList {
+    int choice;
+    public static void purchase(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please select your item :");
+        int choice = sc.nextInt();
+
+
+        }
+
+    }
