79f329433134011ebe2a98819ef9dbeb91731ebb
tx <leeth-wm22@student.tarc.edu.my>
2023-09-20 21:35:01 +0800

    tx

---
+++ OOPTASM/src/staffLogin.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class staffLogin {
+
+    public static void main() {
+
+        // Define the correct username and password
+        String adminUsername = "admin";
+        String adminPassword = "12345";
+
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        boolean loggedIn = false; // Flag to track login status
+
+        while (!loggedIn) {
+            // Prompt the admin for their username
+            System.out.print("Enter your username: ");
+            String username = scanner.nextLine();
+
+            // Prompt the admin for their password
+            System.out.print("Enter your password: ");
+            String password = scanner.nextLine();
+
+            // Check if the entered username and password match the correct credentials
+            if (username.equals(adminUsername) && password.equals(adminPassword)) {
+                loggedIn = true; // Set the flag to true for successful login
+                System.out.println("Access granted. Welcome to the admin main page!");
+                // Call the main menu function here
+                mainMenu.main();
+            } else {
+                System.out.println("Access denied. Incorrect username or password.");
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
+
+    }
+}
